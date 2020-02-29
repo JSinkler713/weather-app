a71a7cf07e1a5d73331eaa529802002a5b3e4f0a
James Sinkler <jamessinkler713@gmail.com>
2020-02-29 12:53:12 -0800

    adds components directory with CurrentWeather component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+// connect with react-redux to store
+import { connect } from 'react-redux';
+import CurrentWeather from './components/CurrentWeather';
 
-function App() {
+
+
+function App(props) {
   return (
     <div>
       <h1>Weather App</h1>
+      <CurrentWeather {...props} />
     </div>
   );
 }
---
+++ src/components/CurrentWeather.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+const CurrentWeather = (props) => {
+  
+  return (
+    <div>
+      <h2>Current Weather </h2>
+    </div>
+  );
+}
+
+export default CurrentWeather;
+     
