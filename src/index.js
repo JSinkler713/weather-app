2320d31cac7495e52ee56428a1c7e64648e706f4
James Sinkler <jamessinkler713@gmail.com>
2020-02-29 12:40:53 -0800

    removes service-worker reference

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import configureStore from './redux/store/configureStore';
 
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 const store = configureStore();
 
