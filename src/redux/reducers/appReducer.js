18d1e98ece114d85b10238c54736c3a9b4c7f07c
James Sinkler <jamessinkler713@gmail.com>
2020-02-29 14:04:26 -0800

    message to self about using THUNK for async

--- src/redux/reducers/appReducer.js
+++ src/redux/reducers/appReducer.js
@@ -12,7 +12,12 @@ export default function appReducer(state = initialState, action) {
     // Make cases here
     case GET_WEATHER:
       console.log('trying to get weather')
-      // do fetch request to get weather
+      let url = 'api.openweathermap.org/data/2.5/weather?q=marin&appid=394feb0696d7821f8fefccc54a73f174'
+      //not going to be able to use fetch here because these are pure functions, whereas this returns a promise - async, going to try and use THUNK
+
+      fetch(url, {method: 'GET'})
+        .then(response => response.json())
+      return state;
     default:
       return state;
   }
